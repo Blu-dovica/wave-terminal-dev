0b070035ca661534acd948b3db516ecd631558a0
melgatto <melaniaesposito94@gmail.com>
2024-03-09 19:07:46 +0100

    fix terminal wrapping bug

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,8 @@ $(function() {
        },
     },
     {
-        greetings: "Hello"
+        greetings: "Hello",
+        wrap: true
     });
  });
 
@@ -23,19 +24,28 @@ $(function() {
 
  type = async function(text, paginate = true) {
     var split = text.split('\n')
-    var term = $.terminal.active();
     console.log(split)
+    var terminal = $.terminal.active();
+    var lastWasEmpty = false;
     for (var i in split) {
-        var isEmpty = split[i].trim().length == 0
+        var isEmpty = !split[i] || split[i].trim().length == 0
         if (!isEmpty) {
-            await term.echo(split[i], { typing: true, delay: 20 })
+            await innerType(terminal, split[i])
         }
-        if (paginate && isEmpty) {
-            var c = await term.read("Continue? (Y/n)")
-            if (c != 'Y' & c != 'y' & c != '') break;
+        else {
+            if (paginate && lastWasEmpty) {
+                var c = await terminal.read("Continue? (Y/n) ")
+                if (c != 'Y' & c != 'y' & c != '') break;
+            }
+            await innerType(terminal, " ")
         }
-        await term.echo(" ")
+        terminal.resize();
+        lastWasEmpty = isEmpty;
     }
+ }
+
+ innerType = function (terminal, text) {
+    return terminal.echo(text, { typing: true, delay: 10 })
  }
  
 
@@ -53,7 +63,7 @@ $(function() {
     return ["File not found."]
  }
 
- var test = "Lorem ipsum dolor sit amet, consectetur\n\nadipiscing elit. Morbi sit amet mollis orci, \nultricies feugiat nisl. \nDonec ut iaculis nulla. Proin condimentum et lectus in sollicitudin. Nullam nec risus eget est varius auctor eu a urna. Cras pretium leo ipsum, a sodales lorem lobortis a. Nullam vel tincidunt dui, molestie sollicitudin orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus non erat rhoncus, tristique arcu eget, condimentum erat. In ullamcorper purus non nisi maximus sollicitudin. Donec eu porttitor magna. Maecenas sit amet arcu ex. Praesent condimentum tempor posuere. Nunc eget consequat tellus. Morbi vel lacus non ligula ornare consectetur id ut dolor. Proin condimentum tempor quam eu interdum. Vivamus eu augue sit amet ligula rutrum venenatis quis nec magna.\n"+
+ var test = "Lorem ipsum [[!s]dolor] sit amet, consectetur\n\nadipiscing elit. Morbi sit amet mollis orci, \nultricies feugiat nisl. \n\n\nDonec ut iaculis nulla. Proin condimentum et lectus in sollicitudin. Nullam nec risus eget est varius auctor eu a urna. Cras pretium leo ipsum, a sodales lorem lobortis a. Nullam vel tincidunt dui, molestie sollicitudin orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus non erat rhoncus, tristique arcu eget, condimentum erat. In ullamcorper purus non nisi maximus sollicitudin. Donec eu porttitor magna. Maecenas sit amet arcu ex. Praesent condimentum tempor posuere. Nunc eget consequat tellus. Morbi vel lacus non ligula ornare consectetur id ut dolor. Proin condimentum tempor quam eu interdum. Vivamus eu augue sit amet ligula rutrum venenatis quis nec magna.\n"+
  "Curabitur condimentum efficitur ultricies. Morbi vehicula sed felis vitae rhoncus. Mauris id porttitor tellus. Mauris tempor elementum nunc, a gravida lectus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus sit amet consectetur mauris. Nam rhoncus mauris ut nulla vestibulum congue.\n\n"+
  "Suspendisse a enim nec mauris facilisis varius feugiat pulvinar dui. Etiam ut maximus diam. Curabitur feugiat nunc nec nulla efficitur cursus. Integer auctor lacus vel efficitur tincidunt. Vestibulum sollicitudin nunc ac neque maximus, sit amet fringilla nunc fermentum. Nulla vel feugiat augue. Donec vitae scelerisque sem. Aliquam varius libero ut tincidunt dapibus. Vestibulum sodales ornare commodo. Nulla vulputate turpis eget rutrum pellentesque. Nulla vel nibh sem. Etiam nec consequat augue, at vehicula risus. Fusce tincidunt lorem erat, non vulputate mauris venenatis sed. Suspendisse egestas volutpat sem, sit amet ornare lectus sagittis sed. Ut nulla erat, tristique et lectus eu, porttitor ultrices tellus. Fusce posuere, est tincidunt egestas semper, ligula lacus feugiat nisl, a pharetra purus ipsum fringilla tortor. \n " +
  "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque vulputate mauris ac nisi dignissim, sit amet ultrices quam pharetra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis congue neque, et eleifend purus finibus eu. Ut porttitor ac nisi at mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla feugiat, lacus sed aliquam molestie, sapien lorem lobortis dolor, quis bibendum urna lorem ac nisi. Vestibulum gravida lacinia sodales. Vivamus nec mollis libero. Morbi in tortor pretium, tincidunt odio in, blandit justo. \n"+
